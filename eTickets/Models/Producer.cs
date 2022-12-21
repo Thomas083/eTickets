bfd431f7703c0562fb176994928f86af437a200b
Thomas <thomasdebray83@gmail.com>
2022-12-21 18:13:12 +0100

    Model relationships

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -5,9 +5,14 @@ namespace eTickets.Models
     public class Producer
     {
         [Key]
-        public int ProducerId { get; set; }
+        public int Id { get; set; }
+
         public string ProfilePictureUrl { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
+        //Relationships
+
+        public List<Movie> Movies { get; set; }
     }
 }
